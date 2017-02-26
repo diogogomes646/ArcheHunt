543e77a10b2763f60fbd6106b5e4dfca4199aade
Panayotis <panlampit1998@hotmail.com>
2017-02-26 17:51:30 +0200

    Bug Fixing
    To Do:
    Fix Enemy Health
    Fix Start & End Position
    Fix Fireball Projection
    Set Up Level UI

---
+++ Assets/Scripts/EndLevel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class EndLevel : MonoBehaviour
+{
+    public Vector3 StartPos;
+    public GameObject Spawn;
+    public GameObject StartPlane;
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.name.Contains("Player"))
+        {
+            Destroy(other.gameObject);
+            StartPos = StartPlane.transform.position;
+            Instantiate(Spawn, new Vector3(StartPos.x, StartPos.y + 2, StartPos.z), Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Scripts/StartLevel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartLevel : MonoBehaviour
+{
+    public Vector3 StartPos;
+
+    public GameObject Spawn;
+    public GameObject StartPlane;
+    void Start()
+    {
+        StartPos = StartPlane.transform.position;
+
+       // Instantiate(Spawn, new Vector3(StartPos.x, StartPos.y + 2, StartPos.z), Quaternion.identity);
+    }
+
+}
