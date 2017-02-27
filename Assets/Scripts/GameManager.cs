760be12c1b32aaa63071d99526ec919b566880d8
Panayotis <panlampit1998@hotmail.com>
2017-02-27 19:52:10 +0200

    UI_Menus
    Added a main Menu Scene
    Added Pause button

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Canvas GameplayCanvas;
+    public GameObject PauseButton;
+    public GameObject PausePanel;
+
+
+
+    public void Start()
+    {
+        Resume();
+    }
+    public void Pause()
+    {
+        PausePanel.SetActive(true);
+        PauseButton.SetActive(false);
+        Time.timeScale = 0;
+    }
+
+    public void Resume()
+    {
+        PausePanel.SetActive(false);
+        PauseButton.SetActive(true);
+        Time.timeScale = 1;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void QuitOn()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public Canvas MainCanvas;
+    public Canvas OptionsCanvas;
+    public Canvas ShopCanvas;
+    public Canvas SelectMenu;
+
+    void Awake()
+    {
+        OptionsCanvas.enabled = false;
+        ShopCanvas.enabled = false;
+
+    }
+
+    public void OptionsOn()
+    {
+        OptionsCanvas.enabled = true;
+        MainCanvas.enabled = false;
+    }
+
+    public void ShopOn()
+    {
+        ShopCanvas.enabled = true;
+        OptionsCanvas.enabled = false;
+        MainCanvas.enabled = false;
+    }
+
+    public void ReturnOn()
+    {
+        OptionsCanvas.enabled = false;
+        ShopCanvas.enabled = false;
+        MainCanvas.enabled = true;
+    }
+
+    public void PlayOn()
+    {
+
+        SceneManager.LoadScene(1);
+
+
+    }
+
+    public void QuitOn()
+    {
+        Application.Quit();
+    }
+}
